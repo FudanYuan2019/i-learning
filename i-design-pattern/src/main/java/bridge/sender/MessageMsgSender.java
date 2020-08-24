2812a73713361b755829977c25cddda93572c838
FudanYuan2019 <18212010046@fudan.edu.cn>
2020-08-24 11:49:51 +0800

    add private

--- i-design-pattern/src/main/java/bridge/sender/MessageMsgSender.java
+++ i-design-pattern/src/main/java/bridge/sender/MessageMsgSender.java
@@ -8,7 +8,7 @@ import java.util.List;
  */
 public class MessageMsgSender implements MsgSender {
 
-    List<String> phoneList;
+    private List<String> phoneList;
 
     public MessageMsgSender(List<String> phoneList) {
         this.phoneList = phoneList;
--- i-design-pattern/src/main/java/bridge/sender/PhoneMsgSender.java
+++ i-design-pattern/src/main/java/bridge/sender/PhoneMsgSender.java
@@ -8,7 +8,7 @@ import java.util.List;
  */
 public class PhoneMsgSender implements MsgSender {
 
-    List<String> phoneList;
+    private List<String> phoneList;
 
     public PhoneMsgSender(List<String> phoneList) {
         this.phoneList = phoneList;
