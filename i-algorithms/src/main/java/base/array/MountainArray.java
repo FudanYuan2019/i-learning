c6e0175fca3fbdbac73df415680b920e2cfc7acb
FudanYuan2019 <18212010046@fudan.edu.cn>
2020-11-03 11:13:48 +0800

    add algo

--- i-algorithms/src/main/java/base/array/MountainArray.java
+++ i-algorithms/src/main/java/base/array/MountainArray.java
@@ -6,9 +6,9 @@ import util.PrintUtil;
  * @Author: Jeremy
  * @Date: 2020/10/25 12:21
  */
-public class LongestMountain {
+public class MountainArray {
     public static void main(String[] args) {
-        LongestMountain mountain = new LongestMountain();
+        MountainArray mountain = new MountainArray();
         int[] A = new int[]{2, 1, 4, 7, 3, 2, 5};
         int res;
         res = mountain.longestMountainV1(A);
@@ -16,6 +16,61 @@ public class LongestMountain {
 
         res = mountain.longestMountainV2(A);
         PrintUtil.print(res);
+
+        A = new int[]{14, 82, 89, 84, 79, 70, 70, 68, 67, 66, 63, 60, 58,
+                54, 44, 43, 32, 28, 26, 25, 22, 15, 13, 12, 10, 8, 7, 5, 4, 3};
+        boolean valid = mountain.validMountainArray(A);
+        PrintUtil.print(valid);
+    }
+
+    /**
+     * 941. 有效的山脉数组
+     * 给定一个整数数组 A，如果它是有效的山脉数组就返回 true，否则返回 false。
+     * <p>
+     * 让我们回顾一下，如果 A 满足下述条件，那么它是一个山脉数组：
+     * <p>
+     * A.length >= 3
+     * 在 0 < i < A.length - 1 条件下，存在 i 使得：
+     * A[0] < A[1] < ... A[i-1] < A[i]
+     * A[i] > A[i+1] > ... > A[A.length - 1]
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：[2,1]
+     * 输出：false
+     * 示例 2：
+     * <p>
+     * 输入：[3,5,5]
+     * 输出：false
+     * 示例 3：
+     * <p>
+     * 输入：[0,3,2,1]
+     * 输出：true
+     *
+     * @param A
+     * @return
+     */
+    public boolean validMountainArray(int[] A) {
+        int n = A.length;
+        if (n < 3) {
+            return false;
+        }
+        int i = 0;
+        boolean up = false, down = false;
+        while (i < n) {
+            while (i + 1 < n && A[i] < A[i + 1]) {
+                i++;
+                up = true;
+            }
+            while (i + 1 < n && A[i] > A[i + 1]) {
+                i++;
+                down = true;
+            }
+            if (++i < n) {
+                return false;
+            }
+        }
+        return up && down;
     }
 
     /**
